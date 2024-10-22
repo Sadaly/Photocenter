9216e64966b4de76a3771dbabfbb5b27d34832e1
Sadaly <mm12112004@mail.ru>
2024-10-22 20:30:39 +0300

    --

---
+++ Фотоцентр/Components/Classes/UserRoleState.cs
@@ -0,0 +1,26 @@
+﻿namespace Фотоцентр.Components.Classes
+{
+    public class UserRoleState
+    {
+        public Role CurrentRole { get; private set; } = Role.none;
+
+        public void SetRole(Role role)
+        {
+            CurrentRole = role;
+        }
+
+        public bool IsAdmin()
+        {
+            return CurrentRole == Role.admin;
+        }
+    }
+
+    public enum Role
+    {
+        none,
+        admin,
+        client,
+        manager,
+        photographer,
+    }
+}
